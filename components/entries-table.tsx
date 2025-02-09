c14f193a9de9bcf772773d8abd3b0b171be1e09d
arkadiusz22 <arek.zawadzki.22@gmail.com>
2025-02-09 19:47:12 +0100

    add average indication

--- components/entries-table.tsx
+++ components/entries-table.tsx
@@ -14,25 +14,40 @@ import { getEntries } from "@/lib/actions";
 export async function EntriesTable() {
   const entries = await getEntries();
 
+  const average =
+    entries.length > 2
+      ? entries.reduce((acc, { weight }) => acc + weight, 0) / entries.length
+      : undefined;
+
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Date</TableHead>
-          <TableHead className="text-right">Weight</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {entries.map((entry) => {
-          const utcDate = new Date(entry.date);
-          return (
-            <TableRow key={entry.date}>
-              <TableCell>{format(utcDate, "PPP")}</TableCell>
-              <TableCell className="text-right">{entry.weight}</TableCell>
-            </TableRow>
-          );
-        })}
-      </TableBody>
-    </Table>
+    <div>
+      {!!average && (
+        <div className="mb-6 text-center">
+          Your avarage from last
+          <span className="font-semibold"> {entries.length} </span>
+          days is
+          <span className="font-semibold"> {average.toFixed(2)}</span>.
+        </div>
+      )}
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Date</TableHead>
+            <TableHead className="text-right">Weight</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {entries.map((entry) => {
+            const utcDate = new Date(entry.date);
+            return (
+              <TableRow key={entry.date}>
+                <TableCell>{format(utcDate, "PPP")}</TableCell>
+                <TableCell className="text-right">{entry.weight}</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </div>
   );
 }
