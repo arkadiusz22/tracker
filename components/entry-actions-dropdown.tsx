13c293baa6b8299673bad9992532404a4a6e7d97
arkadiusz22 <arek.zawadzki.22@gmail.com>
2025-02-13 19:36:27 +0100

    add remove entry are you sure dialog

--- components/entry-actions-dropdown.tsx
+++ components/entry-actions-dropdown.tsx
@@ -10,42 +10,85 @@ import { MoreHorizontal } from "lucide-react";
 import { Button } from "./ui/button";
 import { Entry } from "@/lib/types";
 import { removeEntry, updateEntry } from "@/lib/actions";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { useToast } from "@/hooks/use-toast";
 
 export function EntryActionsDropdown({ entry }: { entry: Entry }) {
+  const { toast } = useToast();
+
   const formData = new FormData();
   formData.append("id", entry.id);
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="h-8 w-8 p-0">
-          <span className="sr-only">Open menu</span>
-          <MoreHorizontal />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem
-          onClick={() => {
-            // TODO: add edit popup, get new data and display toast
-            formData.append(
-              "weight",
-              (entry.weight + (Math.random() - 0.5) * 5).toFixed(2).toString(),
-            );
-            formData.append("date", entry.date);
-            updateEntry(formData);
-          }}
-        >
-          Edit
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          onClick={() => {
-            // TOODO: add confirmation popup and display toast
-            removeEntry(formData);
-          }}
-        >
-          Delete
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <AlertDialog>
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="h-8 w-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontal />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem
+            onClick={() => {
+              // TODO: add edit popup, get new data and display toast
+              formData.append(
+                "weight",
+                (entry.weight + (Math.random() - 0.5) * 5)
+                  .toFixed(2)
+                  .toString(),
+              );
+              formData.append("date", entry.date);
+              updateEntry(formData);
+            }}
+          >
+            Edit
+          </DropdownMenuItem>
+          <AlertDialogTrigger asChild>
+            <DropdownMenuItem>Delete</DropdownMenuItem>
+          </AlertDialogTrigger>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete selected
+            entry.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={async () => {
+              try {
+                await removeEntry(formData);
+                toast({
+                  title: "Entry deleted.",
+                });
+              } catch (error) {
+                toast({
+                  title: "Something went wrong.",
+                  description:
+                    error instanceof Error ? error.message : undefined,
+                });
+              }
+            }}
+          >
+            Yes. Delete
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
